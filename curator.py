efbf2ad704fb7065e903ca128424f88fbb4d0288
Wombatlord <jordan.stone0@gmail.com>
2022-10-22 01:27:43 +0100

    Expanded dataclasses

--- curator.py
+++ curator.py
@@ -2,7 +2,7 @@ import urllib3
 import os
 import json
 from dotenv import load_dotenv
-from data import Gallery, Painting
+from data import Archive, Painting, People
 
 load_dotenv()
 
@@ -16,11 +16,12 @@ r = http.request('GET', 'https://api.harvardartmuseums.org/object',
                      'apikey': apiKey,
                      'yearmade': year,
                      # 26 is the classification id for paintings in Harvard Art Museum data.
-                     'classification': '26',
+                     'classification': 26,
                      # how many items in a response per page.
-                     'size': '50',
+                     'size': 50,
+                     'hasimage': 1,
                      # fields we want included in the response.
-                     'fields': 'objectnumber,title,dated,datebegin,dateend,url,people'
+                     'fields': 'objectnumber,title,dated,datebegin,dateend,url,people,accessionyear,medium,primaryimageurl'
                  })
 
 data = json.loads(r.data.decode('utf-8'))
@@ -32,9 +33,27 @@ data_formatted = json.dumps(data, indent=2)
 #         name = entry["people"][0]["name"]
 #         print(entry["title"] + ": " + name + ": " + entry["dated"])
 
-myGallery = Gallery.parse(data)
+currentArchive = Archive.parse(data)
+# print(currentArchive.records[0]["people"])
 
-for painting in myGallery.records:
-    paintings = Painting.parse(painting)
 
-    print(paintings.title)
+def exhibit_index(archive: Archive):
+    for i, painting in enumerate(archive.records):
+        paintings = Painting.parse(painting)
+
+        if paintings.strict_date == True:
+            title = paintings.title
+            artist = People.parse(archive.records[i]["people"])
+            date = paintings.dated
+            medium = paintings.medium
+            url = paintings.url
+            imageurl = paintings.primaryimageurl
+            year_bought = paintings.accessionyear
+
+            print(f"{title}: {artist.name}: {date}\n{medium}\n{url}\n{imageurl}\nacquired: {year_bought}\n")
+
+            # print(paintings.title + ": " + artist.name + ": " + paintings.dated + "\n" + paintings.medium +
+            #       "\n" + paintings.url + "\n" + str(paintings.primaryimageurl) + "\n" + str(paintings.accessionyear) + "\n")
+
+
+exhibit_index(currentArchive)
--- data.py
+++ data.py
@@ -3,12 +3,12 @@ from dataclasses import dataclass
 
 
 @dataclass
-class Gallery:
+class Archive:
     info: dict
     records: list[dict]
 
     @classmethod
-    def parse(cls, data: dict) -> Gallery:
+    def parse(cls, data: dict) -> Archive:
         kwargs = {
             "info": data["info"],
             "records": data["records"]
@@ -18,23 +18,48 @@ class Gallery:
 
 @dataclass
 class Painting:
+    accessionyear: str
     objectnumber: str
     title: str
     dated: str
     datebegin: int
     dateend: int
-    # url: str
-    # people: list[dict]
+    url: str
+    medium: str
+    primaryimageurl: str
 
     @classmethod
-    def parse(cls, data: Gallery):
+    def parse(cls, data: Archive):
         kwargs = {
             "objectnumber": data["objectnumber"],
             "title": data["title"],
             "dated": data["dated"],
             "datebegin": data["datebegin"],
             "dateend": data["dateend"],
-            # "url": data[""],
-            # people: list[dict]
+            "url": data["url"],
+            "accessionyear": data["accessionyear"],
+            "medium": data["medium"],
+            "primaryimageurl": data["primaryimageurl"]
+        }
+        return cls(**kwargs)
+
+    @property
+    def strict_date(self) -> bool:
+        return self.datebegin == self.dateend
+
+@dataclass
+class People:
+    role: str
+    name: str
+    gender: str
+    culture: str
+
+    @classmethod
+    def parse(cls, data: Painting):
+        kwargs = {
+            "role": data[0]["role"],
+            "name": data[0]["name"],
+            "gender": data[0]["gender"],
+            "culture": data[0]["culture"]
         }
         return cls(**kwargs)
