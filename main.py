8d96be2f7824860f8f5e7008bf74abef76c921f0
Wombatlord <jordan.stone0@gmail.com>
2022-10-27 16:45:29 +0100

    Method extractions from Main to appropriate classes.

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-import urllib3
 import os
 import json
 import sys
 from dotenv import load_dotenv
-from curator import Curator
-from data import Archive, Artifact, People
-from curator import Curator
+from src.adaptors.curator import Curator
 
-load_dotenv()
-
-apiKey = os.environ.get("KEY")
-http = urllib3.PoolManager()
-
-year = 1990
 raw_data: list[bytes] = []
-
-
-def get_raw(key: str, page: int, year_: int) -> dict:
-    r = http.request('GET', 'https://api.harvardartmuseums.org/object',
-                     fields={
-                         'apikey': key,
-                         'yearmade': year_,
-                         'page': page,
-                         # 26 is the classification id for paintings in Harvard Art Museum data.
-                         'classification': '26',
-                         # how many items in a response per page.
-                         'size': 5,
-                         'hasimage': 1,
-                         # fields we want included in the response.
-                         'fields': 'objectnumber,title,dated,datebegin,dateend,url,people,accessionyear,medium,primaryimageurl,imagepermissionlevel,id'
-                     })
-    raw = r.data
-    raw_data.append(raw)
-    return json.loads(r.data.decode())
-
 
 def get_raw_fixture(_: str, page: int, __: int) -> dict:
     print("FIXTURE\n")
@@ -42,25 +13,27 @@ def get_raw_fixture(_: str, page: int, __: int) -> dict:
 
 test_mode = "-t" in sys.argv
 
+curator = Curator()
+
 # Swaps out retrieve implementation for testing against dumped snapshots
 match test_mode:
     case True:
         retrieve = get_raw_fixture
     case False:
-        retrieve = get_raw
+        retrieve = curator.prepare_sources(["HAM"], {"year": 1990})
 
 
-currentArchive = Archive.parse(retrieve(apiKey, 1, year))
+exhibit = curator.curate_exhibit(retrieve)
+print(exhibit)
 
-curator = Curator(1990, apiKey, retrieve)
-curatorGallery = curator.exhibit_index(currentArchive, exhibit=[])
+# curatorGallery = curator.exhibit_index(currentArchive, exhibit=[])
 
-print(f"There are {len(curatorGallery)} objects in your gallery. \n")
+# print(f"There are {len(curatorGallery)} objects in your gallery. \n")
 
-for i, item in enumerate(curatorGallery):
-    print(str(i) + ": " + item)
+# for i, item in enumerate(curatorGallery):
+#     print(str(i) + ": " + item)
 
-print(currentArchive.info)
+# print(currentArchive.info)
 
 # Dump raw data snapshots if -d or --dump appear in command line args
 dump_mode = set(sys.argv) & {"-d", "--dump"} != set()
