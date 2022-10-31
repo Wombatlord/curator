efa5fea3f3972989cb2a07090b819e7ec95a1f0e
Wombatlord <jordan.stone0@gmail.com>
2022-10-31 20:59:29 +0000

    type hinting

--- src/adaptors/museum.py
+++ src/adaptors/museum.py
@@ -1,12 +1,12 @@
 from enum import Enum
-import typing
-from src.adaptors.source import Result
+
+from src.adaptors.source import Result, Source
 from src.adaptors.harvard_art_museum import Source as _HAM_Source
 
 class Museum(Enum):
     HAM = "harvard_art_museum"
 
-    def get_source(self) -> typing.Sequence[Result]:
+    def get_source(self) -> type[Source]:
         match self:
             case Museum.HAM:
                 return _HAM_Source
